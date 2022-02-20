a0273beb8f3812164431b6ca2e18bbbaa924fd1c
Stanisław Pawlaczyk <ctahncnab@gmail.com>
2022-02-20 19:09:37 +0100

    Add pages and settings

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,21 @@
-/*const app = {
+/*import { settings } from './settings.js'
+
+const app = {
+  initData: function(){
+    const url = settings.db.url + '/' + settings.db.products;
+    this.data = {};
+    fetch(url)
+      .then((rawResponse) => {
+        return rawResponse.json();
+      })
+      .then((parsedResponse) => {
+        this.data.products = parsedResponse;
+      });
+  },
+
   init: fucntion(){
+    const thisApp = this;
+    thisApp.initData();
   },
 }
 
